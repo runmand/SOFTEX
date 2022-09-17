318ead39bb9bb5140985a21a0838832a046d3937
runmand <78941772+runmand@users.noreply.github.com>
2022-09-17 20:48:41 -0300

    Create App.js

---
+++ REACT/amanda/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./Components";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>AMANDA CHAVES</h1>
+      <h2>COMPONENTE</h2>
+    </div>
+  );
+}
